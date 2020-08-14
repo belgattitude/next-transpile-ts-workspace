84d191a19bfe20e9698cb0b4c7fd20b4669d0cc2
Sébastien Vanvelthem <belgattitude@gmail.com>
2020-08-14 16:15:31 +0200

    refactor with src directory

--- packages/bar/src/async-message.tsx
+++ packages/bar/src/async-message.tsx
@@ -1,7 +1,12 @@
-import {useEffect, useState} from "react";
+import React, {FC, useEffect, useState} from "react";
 import {Message} from "./message";
 
-export const AsyncMessage = (props: {apiUrl: '/api/hello'}) => {
+type Props = {
+    apiUrl: string
+    children?: never
+}
+
+export const AsyncMessage: FC<Props> = (props) => {
     const [msg, setMsg] = useState<string>('...');
     useEffect(() => {
         fetch(props.apiUrl).then(res => res.text()).then((res) => {
--- packages/bar/src/message.tsx
+++ packages/bar/src/message.tsx
@@ -1,6 +1,9 @@
+import React from "react";
+
 type Props = {
     message: string;
+    children?: never;
 }
 
-export const Message = (props: Props) => <span>{props.message}</span>;
+export const Message: React.FC<Props> = (props) => <span>{props.message}</span>;
 
