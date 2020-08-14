ad62ec16c69d488698bf61121a9fb1f96532cdcc
Sébastien Vanvelthem <belgattitude@gmail.com>
2020-08-14 13:31:33 +0200

    with scoped packages

--- packages/web-app/next.config.js
+++ packages/web-app/next.config.js
@@ -1,5 +1,5 @@
 // Tell webpack to compile the "bar" package, necessary if you're using the export statement for example
 // https://www.npmjs.com/package/next-transpile-modules
-const withTM = require('next-transpile-modules')(['bar', 'foo'])
+const withTM = require('next-transpile-modules')(['@package/bar', '@package/foo'])
 
 module.exports = withTM({})
